0920b096e18125d96fc7a8c485b198c15ee76ffe
Mary Margaret <mmlawton15@gmail.com>
2021-12-02 10:34:53 -0500

    added pseudocode and section for the columns in the scheduler

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,7 +1,7 @@
 // DONE - GIVEN I am using a daily planner to create a schedule
 // DONE - WHEN I open the planner
 // DONE - THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
+// DONE - WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
@@ -13,8 +13,10 @@
 // THEN the saved events persist
 
 
-//GET THE CURRENT TIME
+//CURRENT TIME
 document.getElementById("currentDay").textContent += moment().format('dddd, MMMM Do YYYY');
+
+//COLUMNS FOR THE SCHEDULER
 
 
 
