0c771d34b525d77a4c86a5301e53f5b416f268dc
Mary Margaret <mmlawton15@gmail.com>
2021-12-02 18:59:52 -0500

    figuring out layouts and colors needed for the module before jumping into logic

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -2,8 +2,8 @@
 // DONE - WHEN I open the planner
 // DONE - THEN the current day is displayed at the top of the calendar
 // DONE - WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
+// DONE - THEN I am presented with time blocks for standard business hours
+// DONE - WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click into a time block
 // THEN I can enter an event
