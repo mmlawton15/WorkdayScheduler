69412b41f6898b6b5b70375715496ed9f4ddd6ed
Mary Margaret <mmlawton15@gmail.com>
2021-12-05 19:41:07 -0500

    added screenshots and trying to figure out local storage

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -41,12 +41,10 @@ function getCurrentTime() {
 }
 
 //SAVE THE DATA ENTERED INTO LOCAL STORAGE
-//add an event listener to the save button so when the save button is clicked, it sends the value in the div to local storage
+//when the save button is clicked, it sends the value in the div to local storage
 function saveTasksToLocalStorage(event) {
-    //when the save button is clicked, take the text from the div and save it to local storage
-    console.log("save button was clicked"); //check the button works
     for (var i = 9; i<18; i++) {
-        var currentTaskElementValue = document.getElementById("textArea" + i); //similar code to the colorChangeBasedOnTime to get the text content of each task div
+        var currentTaskElementValue = document.getElementById("textArea" + i); //get the text content of each task div
         if (currentTaskElementValue !== null) { //if the text in the div element has substance
             localStorage.setItem('task', JSON.stringify(currentTaskElementValue)); //convert that input into a string and save it as a task in local storage
             currentTaskElementValue.textContent = JSON.parse(localStorage.getItem('task')); //turn that item back into an object and make it appear on the screen by turning it into the text content of the div
@@ -70,4 +68,4 @@ for (var i = 0; i<arrayOfButtons.length; i++){
 
 getCurrentTime();
 colorChangeBasedOnTime();
-//saveTasksToLocalStorage();
+saveTasksToLocalStorage();
