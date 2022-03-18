43d4d93cc0f0303e52444537e87dfde83b7f6f5e
Mary Margaret <mmlawton15@gmail.com>
2022-03-18 10:41:04 -0400

    working on localstorage

--- assets/script.js
+++ assets/script.js
@@ -33,21 +33,25 @@ function getCurrentTime() {
 
 //SAVE THE DATA ENTERED INTO LOCAL STORAGE
 function saveTasksToLocalStorage(event) {
-
-    //for (var i = 9; i<18; i++) {
-        // use the event to find out which button was clicked
+    for (var i = 9; i<18; i++) {
+        //use the event to find out which button was clicked
 
         // reach into the html to grab that button corresponding text area
-        var currentTaskElementValue = document.getElementById("textArea-"); //get the text content of each task div
-        console.log(currentTaskElementValue) // sanity check
-        if (currentTaskElementValue !== null) { //if the text in the div element has substance
+        var currentTaskElementValue = document.getElementById("textArea-"+i);
+        console.log(currentTaskElementValue)
+        if (currentTaskElementValue !== null) {
             localStorage.setItem('task', JSON.stringify(currentTaskElementValue)); //convert that input into a string and save it as a task in local storage
             currentTaskElementValue.textContent = JSON.parse(localStorage.getItem('task')); //turn that item back into an object and make it appear on the screen by turning it into the text content of the div
             console.log(currentTaskElementValue);
             //localStorage.task = currentTaskElementValue.textContent; //set local storage.task key to be the value of the div element
         }
-    //}
+    }
 }
+
+//I want the function to be this simple
+// var saveTasksToLocalStorage = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+// };
 
 //FOR LOOP TO FILTER THROUGH ALL THE HTML BUTTONS AND ADD EVENT LISTENERS
 var arrayOfButtons = document.getElementsByClassName("saveBtn");
